af43a1164835bff918335e7c40dc9a15e643c1d9
paleumm <zdotqdot@gmail.com>
2023-02-24 10:30:46 +0700

    Update Learn

--- learn/09-bitehist.py
+++ learn/09-bitehist.py
@@ -9,8 +9,8 @@ prog = '''
 BPF_HISTOGRAM(dist);
 
 int kprobe__blk_account_io_done(struct pt_regs *ctx, struct request *req) {
-    dist.increment(bpf_log2l(req->__data_len / 1024));
-    return 0;
+	dist.increment(bpf_log2l(req->__data_len / 1024));
+	return 0;
 }
 '''
 
